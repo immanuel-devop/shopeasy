aa5238ddf4fef9b834226c0157f635fb3bf695e3
immanuel-devop <immanuel.devop@gmail.com>
2025-03-27 10:04:07 +0530

    Initial Commit: Basic Express app setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require ("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/',(req,res)=>{
+    res.send('Welcome to shop easy')
+})
+
+app.listen(PORT,()=>{
+    console.log(`server running on the port ${PORT}`)
+})
